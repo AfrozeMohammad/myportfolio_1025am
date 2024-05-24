af6d9c6ce260af5ef7718eaa55abcde55270d732
Afroze Mohammad <afroze.mohammad@audreeinfotech.com>
2024-05-24 10:22:30 +0530

    ok

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
+import { HttpHeaders } from '@angular/common/http';
 import { Component } from '@angular/core';
+import { NgForm, NgModel } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +9,10 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'myportfolio';
+
+
+  
+
+
+  
 }
